e466dbbbf33193115a9771f98b3477f397415338
Matthew Prestifilippo <m.prestifilippo@gmail.com>
2016-10-05 10:59:13 +0000

    returning acceptor task

--- lib/ai/cell/standard_graded.ex
+++ lib/ai/cell/standard_graded.ex
@@ -3,36 +3,38 @@ defmodule AI.Cell.StandardGraded do
   Graded cell that when it is stimulated with positive charges, it produces positive charges/trasmitters
   """
   use GenEvent
-  
+
   defstruct [
     subscribers: [],
     threshold: 0.0
   ]
-  
+
   def handle_event({:stimulate, transmitter}, state) do
     {:ok, state}
   end
-  
+
   def handle_call(:subscribers, state) do
     {:ok, state.subscribers, state}
   end
-  
+
   def handle_call({:add_subscriber, subscriber}, state) do
     state = Map.put(state, :subscribers, [subscriber|state.subscribers])
     {:ok, state.subscribers, state}
   end
-  
+
   def start_link do
     state = %__MODULE__{}
     {:ok, pid} = GenEvent.start_link
     :ok = GenEvent.add_handler(pid, __MODULE__, state)
-    Task.start_link(fn ->
+
+    {:ok, task} = Task.start_link(fn ->
       GenEvent.stream(pid)
-      |> Stream.transform(%{}, fn(transmitter, acc) ->
+      |> Stream.transform(%{charges: []}, fn({:stimulate, transmitter}, acc) ->
+        IO.puts "#{inspect pid} transmitttter: #{inspect transmitter}"
         now = :os.timestamp
         # add transmitter to the charges
         acc = case Enum.count(acc.charges) do
-            5 -> 
+            5 ->
                 [_|t] = acc.charges
                 Map.put(acc, :charges, t)
             _ -> acc
@@ -43,31 +45,32 @@ defmodule AI.Cell.StandardGraded do
         charge = Enum.reduce(acc.charges, 0, fn(el, sum) ->
           {t, ts} = el
           diff = :timer.now_diff(now, ts)
-          sum + cond do
+          val = cond do
             diff < 100 -> t
             diff < 500 -> t / 2
             diff < 1000 -> t / 4
             :true -> 0
           end
+          sum + val
         end)
 
         {[charge], acc}
       end)
-      
+
       # timeout 1 milli, send a zero (decay)
       # this will make the gradient continue to send out charges in
       # a gradual decline instead of a hard stop
 
       # relay transmitterssss
-      |> Enum.each(fn(charge) -> 
+      |> Enum.each(fn(charge) ->
         if charge > 0 do
-          {:ok, subscribers} = GenEvent.call(pid, __MODULE__, :subscribers)
+          subscribers = GenEvent.call(pid, __MODULE__, :subscribers)
           num_subscribers = Enum.count(subscribers)
           Enum.each(subscribers, &GenEvent.notify(&1, {:stimulate, charge / num_subscribers}))
         end
       end)
     end)
 
-    pid
+    {pid, task}
   end
 end
--- test/ai/cell/standard_graded_test.exs
+++ test/ai/cell/standard_graded_test.exs
@@ -2,7 +2,7 @@ defmodule AI.Cell.StandardGradedTest do
   use ExUnit.Case
 
   setup do
-    cell = AI.Cell.StandardGraded.start_link
+    {cell, _} = AI.Cell.StandardGraded.start_link
     {:ok, cell: cell}
   end
 
@@ -12,6 +12,17 @@ defmodule AI.Cell.StandardGradedTest do
     assert Enum.count(subscribers) == 1
     assert Enum.at(subscribers, 0) == subscriber
     GenEvent.stop(cell)
+  end
+
+  test "should stimulate the subscriber", %{cell: cell} do
+    {subscriber, _} = AI.Cell.StandardGraded.start_link
+    _ = GenEvent.call(cell, AI.Cell.StandardGraded, {:add_subscriber, subscriber})
+    :timer.sleep(1000)
+    for _ <- 0..100 do
+      :ok = GenEvent.notify(cell, {:stimulate, 10})
+    end
+
+    :timer.sleep(1000)
   end
 
 end
