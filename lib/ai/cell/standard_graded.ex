7460f1f0b2bf7aee1e61a590d807baa7480d10c2
Matthew Prestifilippo <m.prestifilippo@gmail.com>
2016-10-05 11:34:27 +0000

    smaller functions

--- lib/ai/cell/standard_graded.ex
+++ lib/ai/cell/standard_graded.ex
@@ -26,51 +26,57 @@ defmodule AI.Cell.StandardGraded do
     state = %__MODULE__{}
     {:ok, pid} = GenEvent.start_link
     :ok = GenEvent.add_handler(pid, __MODULE__, state)
-
+    task = start_processor(pid)
+    
+    {pid, task}
+  end
+  
+  defp start_processor(pid) do
     {:ok, task} = Task.start_link(fn ->
       GenEvent.stream(pid)
       |> Stream.transform(%{charges: []}, fn({:stimulate, transmitter}, acc) ->
-        IO.puts "#{inspect pid} transmitttter: #{inspect transmitter}"
         now = :os.timestamp
-        # add transmitter to the charges
-        acc = case Enum.count(acc.charges) do
-            5 ->
-                [_|t] = acc.charges
-                Map.put(acc, :charges, t)
-            _ -> acc
-        end
-        acc = Map.put(acc, :charges, acc.charges ++ [{transmitter, now}])
-
-        # calculate current charge
-        charge = Enum.reduce(acc.charges, 0, fn(el, sum) ->
-          {t, ts} = el
-          diff = :timer.now_diff(now, ts)
-          val = cond do
-            diff < 100 -> t
-            diff < 500 -> t / 2
-            diff < 1000 -> t / 4
-            :true -> 0
-          end
-          sum + val
-        end)
+        acc = accumulate_charges(transmitter, acc, now)
+        
+        charge = calculate_charge(acc.charges, now)
 
         {[charge], acc}
       end)
-
-      # timeout 1 milli, send a zero (decay)
-      # this will make the gradient continue to send out charges in
-      # a gradual decline instead of a hard stop
-
-      # relay transmitterssss
-      |> Enum.each(fn(charge) ->
-        if charge > 0 do
-          subscribers = GenEvent.call(pid, __MODULE__, :subscribers)
-          num_subscribers = Enum.count(subscribers)
-          Enum.each(subscribers, &GenEvent.notify(&1, {:stimulate, charge / num_subscribers}))
-        end
-      end)
+      |> Enum.each(&relay(&1, pid))
     end)
-
-    {pid, task}
+    task
+  end
+  
+  defp accumulate_charges(transmitter, acc, now) do
+    # add transmitter to the charges
+    acc = case Enum.count(acc.charges) do
+        5 ->
+            [_|t] = acc.charges
+            Map.put(acc, :charges, t)
+        _ -> acc
+    end
+    Map.put(acc, :charges, acc.charges ++ [{transmitter, now}])
+  end
+  
+  defp calculate_charge(charges, now) do
+    Enum.reduce(charges, 0, fn(el, sum) ->
+      {t, ts} = el
+      diff = :timer.now_diff(now, ts)
+      val = cond do
+        diff < 100 -> t
+        diff < 200 -> t / 2
+        diff < 300 -> t / 4
+        :true -> 0
+      end
+      sum + val
+    end)
+  end
+  
+  defp relay(charge, pid) do
+    if charge > 0 do
+      subscribers = GenEvent.call(pid, __MODULE__, :subscribers)
+      num_subscribers = Enum.count(subscribers)
+      Enum.each(subscribers, &GenEvent.notify(&1, {:stimulate, charge / num_subscribers}))
+    end
   end
 end
